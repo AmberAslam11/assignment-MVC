41555ea8c209fa986b7bdc9b92e01559c0540162
Amber aslam <hareemhooria158@gmailc,onm>
2022-07-14 03:28:01 +0500

    Final Project

--- AMBER/Controllers/HomeController.cs
+++ AMBER/Controllers/HomeController.cs
@@ -1,19 +1,45 @@
-﻿using System.Diagnostics;
+﻿
+using amber.Data;
 using AMBER.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
-namespace AMBER.Controllers
+namespace amber.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly DataContext _context;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, DataContext context)
         {
+            _context = context;
             _logger = logger;
         }
 
+        [HttpPost]
+        public IActionResult check(login l)
+        {
+            foreach (var i in _context.login.ToList())
+                if (i.username == l.username && i.password == l.password)
+                {
+                    ViewBag.message = "SUCCESSFULL";
+                    return View("form");
+                }
+                else
+                {
+                    ViewBag.message = "FAILED";
+                    return View("form");
+                }
+            return View();
+        }
+
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult form()
         {
             return View();
         }
---
+++ AMBER/Data/DataContext.cs
@@ -0,0 +1,13 @@
+﻿using AMBER.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace amber.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<login> login { get; set; }
+    }
+}
